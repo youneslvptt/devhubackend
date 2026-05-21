964895475a582fb139a19f5b1ba8708738c679fb
Younes <younesbj71@gmail.com>
2026-05-21 21:50:23 +0100

    lastpush

--- utils/email.js
+++ utils/email.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false, // true for 465, false for others
+  secure: false, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
